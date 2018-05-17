23c52dd35cf8cfc8682b2ac8bedd6d34c37fef5d
Cong Zhang <dancerphil1994@gmail.com>
2018-05-17 17:23:17 +0800

    update

--- Hacker/index.js
+++ Hacker/index.js
@@ -1,14 +1,12 @@
+let active = false;
 chrome.browserAction.onClicked.addListener(function(tab) {
-	if(open){
-		var thecode = "document.body.setAttribute('contenteditable', true);";
+  active=!active;
+  if(active){
 		chrome.browserAction.setIcon({path:"iconclose.png"});
-		open=!open;
-	} else {
-		var thecode = "document.body.setAttribute('contenteditable', false);";
+  } else {
 		chrome.browserAction.setIcon({path:"icon.png"});
-		open=!open;
-	}
-  chrome.tabs.executeScript({
-    code: thecode
+  }
+  chrome.tabs.executeScript(null, {
+    code: `document.body.setAttribute('contenteditable', ${active});`
   });
 });
